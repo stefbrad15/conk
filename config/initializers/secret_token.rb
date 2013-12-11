4a46039ce077abb22affc1c271ace9113df5f099
stefbrad <stefbrad@rails.(none)>
2013-12-10 20:19:42 -0500

    Initialize repos

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Conk::Application.config.secret_key_base = 'ffaf1e95da778034b6c9a4457fe7d535604071f94c84d9455d2714c8fc99a89ebe2ba0b65305d0334bc44406c730b48750d94b720a947fdc2d4de0ca657eeec5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Conk::Application.config.session_store :cookie_store, key: '_conk_session'
